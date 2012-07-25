c757689bb365fda7652f752c0ab3424747b168e3
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-07-25 19:47:38 +0200

    Rewrote AssertionInserter to not rely on JDTs nullness stuff

--- org.eclipselabs.nullness/src/org/eclipselabs/nullness/NullnessAssertionInserter.java
+++ org.eclipselabs.nullness/src/org/eclipselabs/nullness/NullnessAssertionInserter.java
@@ -15,11 +15,17 @@ import static org.objectweb.asm.Opcodes.*;
 import java.lang.reflect.Field;
 
 import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.compiler.CharOperation;
+import org.eclipse.jdt.core.dom.IAnnotationBinding;
 import org.eclipse.jdt.core.dom.IBinding;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.internal.compiler.lookup.LocalTypeBinding;
-import org.eclipse.jdt.internal.compiler.lookup.TagBits;
+import org.eclipse.jdt.internal.compiler.lookup.MethodBinding;
+import org.eclipse.jdt.internal.compiler.lookup.SyntheticMethodBinding;
+import org.eclipse.jdt.internal.compiler.lookup.TypeBinding;
 import org.eclipse.jdt.internal.compiler.lookup.TypeConstants;
 import org.objectweb.asm.ClassAdapter;
 import org.objectweb.asm.ClassVisitor;
@@ -36,14 +42,91 @@ class NullnessAssertionInserter extends ClassAdapter {
 	private boolean checkInserted = false;
 
 	private final ITypeBinding typeBinding;
+	private final TypeBinding internalBinding;
+	private final Boolean defaultNonNullState;
 
 	NullnessAssertionInserter(ClassVisitor classVisitor, ITypeBinding typeBinding) {
 		super(classVisitor);
 		this.typeBinding = typeBinding;
+		this.internalBinding = getInternalBinding(typeBinding);
+		this.defaultNonNullState = getDefaultNonNullState();
 	}
 
 	boolean isCheckInserted() {
 		return checkInserted;
+	}
+
+	private Boolean getDefaultNonNullState() {
+		Boolean result = recursiveGetDefaultNonNullState(typeBinding);
+		return result;
+	}
+
+	private Boolean getDefaultNonNullState(IBinding binding) {
+		IAnnotationBinding[] annotations = binding.getAnnotations();
+		for (IAnnotationBinding annotation : annotations) {
+			if ("org.eclipse.jdt.annotation.NonNullByDefault".equals(getAnnotationTypeName(annotation))) {
+				// TODO check the value of 'value'
+				return Boolean.TRUE;
+			}
+		}
+		return null;
+	}
+
+	private Boolean getEffectiveNonNullState(IMethodBinding binding) {
+		IAnnotationBinding[] annotations = binding.getAnnotations();
+		for (IAnnotationBinding annotation : annotations) {
+			if ("org.eclipse.jdt.annotation.NonNull".equals(getAnnotationTypeName(annotation))) {
+				// TODO check the value of 'value'
+				return Boolean.TRUE;
+			}
+			if ("org.eclipse.jdt.annotation.NonNullByDefault".equals(getAnnotationTypeName(annotation))) {
+				return Boolean.TRUE;
+			}
+			if ("org.eclipse.jdt.annotation.Nullable".equals(getAnnotationTypeName(annotation))) {
+				return Boolean.FALSE;
+			}
+		}
+		return defaultNonNullState;
+	}
+
+	private Boolean getEffectiveNonNullState(IMethodBinding binding, IAnnotationBinding[] parameterAnnotations) {
+		for (IAnnotationBinding annotation : parameterAnnotations) {
+			if ("org.eclipse.jdt.annotation.NonNull".equals(getAnnotationTypeName(annotation))) {
+				return Boolean.TRUE;
+			}
+			if ("org.eclipse.jdt.annotation.Nullable".equals(getAnnotationTypeName(annotation))) {
+				return Boolean.FALSE;
+			}
+		}
+		Boolean methodDefault = getDefaultNonNullState(binding);
+		if (methodDefault != null)
+			return methodDefault;
+		return defaultNonNullState;
+	}
+
+	private String getAnnotationTypeName(IAnnotationBinding annotation) {
+		return annotation.getAnnotationType().getQualifiedName();
+	}
+
+	private Boolean recursiveGetDefaultNonNullState(IBinding binding) {
+		Boolean result = getDefaultNonNullState(binding);
+		if (result == null) {
+			if (binding instanceof IMethodBinding) {
+				result = recursiveGetDefaultNonNullState(((IMethodBinding) binding).getDeclaringClass());
+			} else if (binding instanceof IVariableBinding) {
+				result = recursiveGetDefaultNonNullState(((IVariableBinding) binding).getDeclaringClass());
+			} else if (binding instanceof ITypeBinding) {
+				ITypeBinding castedBinding = (ITypeBinding) binding;
+				if (castedBinding.isMember()) {
+					result = recursiveGetDefaultNonNullState(castedBinding.getDeclaringClass());
+				} else if (castedBinding.isLocal()) {
+					result = recursiveGetDefaultNonNullState(castedBinding.getDeclaringMethod());
+				} else {
+					result = recursiveGetDefaultNonNullState(castedBinding.getPackage());
+				}
+			}
+		}
+		return result;
 	}
 
 	private <T> T getInternalBinding(IBinding binding) {
@@ -60,64 +143,78 @@ class NullnessAssertionInserter extends ClassAdapter {
 		}
 	}
 
-	@Override
-	public MethodVisitor visitMethod(final int access, final String name, String desc, String signature, String[] exceptions) {
-		final Type[] args = Type.getArgumentTypes(desc);
-
-		MethodVisitor v = cv.visitMethod(access, name, desc, signature, exceptions);
+	private IMethodBinding findMethod(String name, String desc) {
+		char[] descAsArray = desc != null ? desc.toCharArray() : null;
 		IMethodBinding[] methods = typeBinding.getDeclaredMethods();
-		String[] parameterNames = null;
-		org.eclipse.jdt.internal.compiler.lookup.MethodBinding actualMethod = null;
 		try {
 			for (IMethodBinding method : methods) {
 				if (name.equals(method.getName()) || CONSTRUCTOR.equals(name) && method.isConstructor()) {
-					org.eclipse.jdt.internal.compiler.lookup.MethodBinding candidate = getInternalBinding(method);
-					char[] candidateSignature = candidate.signature();
-					boolean equal = desc == null && candidateSignature == null;
-					if (!equal && desc != null && candidateSignature != null) {
-						equal = desc.equals(String.valueOf(candidateSignature));
-					}
-					if (equal) {
-						IMethod javaMethod = (IMethod) method.getJavaElement();
-						if (javaMethod != null) {
-							parameterNames = javaMethod.getParameterNames();
-						}
-						actualMethod = candidate;
-						break;
+					MethodBinding candidate = getInternalBinding(method);
+					if (CharOperation.equals(descAsArray, candidate.signature())) {
+						return method;
 					}
 				}
 			}
+			return null;
 		} catch (Exception e) {
-			// TODO proper exception handling
-			e.printStackTrace();
-			return v;
+			return null;
 		}
-		if (actualMethod == null) {
-			return v;
-		}
-		long nonNullBits = TagBits.AnnotationNonNull | TagBits.AnnotationNonNullByDefault;
-		final boolean nonNull = (actualMethod.getAnnotationTagBits() & nonNullBits) != 0;
-		final Boolean[] nonNullParameters = actualMethod.parameterNonNullness;
-		final boolean[] hasNonNullParameter = new boolean[] { false };
-		if (nonNullParameters != null) {
-			for (Boolean nonNullParameter : nonNullParameters) {
-				if (Boolean.TRUE.equals(nonNullParameter)) {
-					hasNonNullParameter[0] = true;
-					break;
-				}
+	}
+
+	private String[] getParameterNames(IMethodBinding methodBinding) {
+		try {
+			IMethod javaMethod = (IMethod) methodBinding.getJavaElement();
+			if (javaMethod == null) {
+				return new String[0];
 			}
+			return javaMethod.getParameterNames();
+		} catch (JavaModelException e) {
+			return new String[methodBinding.getParameterTypes().length];
 		}
-		if (!hasNonNullParameter[0] && !nonNull) {
-			return v;
+	}
+
+	private boolean hasNonNullParameter(Boolean[] nonNullState) {
+		for (int i = 0; i < nonNullState.length; i++) {
+			if (Boolean.TRUE.equals(nonNullState[i]))
+				return true;
 		}
-		final String[] finalParameterNames = parameterNames;
-		return new MethodAdapter(v) {
+		return false;
+	}
+
+	private boolean isEffectivelySynthetic(IMethodBinding methodBinding) {
+		return methodBinding.isSynthetic() || getInternalBinding(methodBinding) instanceof SyntheticMethodBinding;
+	}
+
+	@Override
+	public MethodVisitor visitMethod(final int access, final String name, String desc, String signature, String[] exceptions) {
+		MethodVisitor methodVisitor = cv.visitMethod(access, name, desc, signature, exceptions);
+		if ((access & ACC_SYNTHETIC) != 0) {
+			return methodVisitor;
+		}
+		IMethodBinding methodBinding = findMethod(name, desc);
+		if (methodBinding == null || isEffectivelySynthetic(methodBinding)) {
+			return methodVisitor;
+		}
+		final Type[] args = Type.getArgumentTypes(desc);
+		Type returnType = Type.getReturnType(desc);
+
+		// null == undefined, false == nullable, true == non-null
+		final Boolean[] nonNullParameters = new Boolean[methodBinding.getParameterTypes().length];
+		final Boolean[] returnTypeNonNull = new Boolean[1];
+
+		for (int i = 0; i < nonNullParameters.length; i++) {
+			nonNullParameters[i] = getEffectiveNonNullState(methodBinding, methodBinding.getParameterAnnotations(i));
+		}
+		returnTypeNonNull[0] = getEffectiveNonNullState(methodBinding);
+		final boolean hasNonNullParameter = hasNonNullParameter(nonNullParameters);
+		final String[] parameterNames = hasNonNullParameter ? getParameterNames(methodBinding) : null;
+		return new MethodAdapter(methodVisitor) {
 			private Label throwLabel;
 			private Label startGeneratedCodeLabel;
 
 			@Override
 			public void visitCode() {
-				if (hasNonNullParameter[0]) {
+				if (hasNonNullParameter) {
 					startGeneratedCodeLabel = new Label();
 					mv.visitLabel(startGeneratedCodeLabel);
 					for (int param = 0; param < nonNullParameters.length; param++) {
@@ -133,7 +230,7 @@ class NullnessAssertionInserter extends ClassAdapter {
 							mv.visitJumpInsn(IFNONNULL, end);
 
 							throwStatement(ILLEGAL_ARGUMENT_EXCEPTION, String.format(
-									"Argument for non-null parameter %s at index %d of %s#%s must not be null", finalParameterNames[param],
+									"Argument for non-null parameter %s at index %d of %s#%s must not be null", parameterNames[param],
 									param, getTypeName(), name), end);
 						}
 					}
@@ -163,7 +260,7 @@ class NullnessAssertionInserter extends ClassAdapter {
 			@Override
 			public void visitInsn(int opcode) {
 				if (opcode == ARETURN) {
-					if (nonNull) {
+					if (Boolean.TRUE.equals(returnTypeNonNull[0])) {
 						mv.visitInsn(DUP);
 						if (throwLabel == null) {
 							Label skipLabel = new Label();
